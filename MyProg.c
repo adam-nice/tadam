8772e7af6d2fedcda64b3de7eb7f922a142bb2b5
Adam Nice <adam.nice@campus.technion.ac.il>
2020-11-20 15:07:54 +0200

    adam1

--- MyProg.c
+++ MyProg.c
@@ -1,11 +1,23 @@
 //#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+typedef struct node {
+    void* element;
+    struct node* next_node;
+}*Node;
+
+typedef struct queue{
+    Node first_node;
+    int num;
+    
+}*Queue;
+
+
+
+
+
+
 int main(){
-    printf("enter a num");
-    int x;
-    scanf("%d", &x);
-    printf("adam");
 
  return 0;
 }
